242a83ff85bdda104feb417a70588dbc5abc6240
Victor <94798862+victorhe33@users.noreply.github.com>
2022-01-19 00:25:36 -0500

    ADD DOM manip and STYLES
    version 1.0
    buttons
    displays
    styling

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let playerScore = 0;
 let computerScore = 0;
 let gamesPlayed = 0;
 let numberGames = 5;
+// let tieCounter = 0;
 
 /* Function simulating computer play: randomly selects "Rock", "Paper",
 or "Scissors". */
@@ -21,76 +22,120 @@ function computerPlay() {
   if (randomNum === 2) {
     computerSelection = "Scissors";
   }
+  //display computer selection
+  computer.textContent = (`Computer plays ${computerSelection}`);
 }
 
 //Function to play a round of RPS.
-function playRound() {
-  playerChoice();
-  computerPlay();
-  gamesPlayed += 1;
+function playRound(id) {
+  //store click ID into playerSelection variable
+  playerSelection = id;
 
-//Logic for single round of game
-  if (playerSelection == computerSelection) {
-
-    alert("You are tied!");
-  } else if (playerSelection == "Rock" && computerSelection == "Scissors"
-    || playerSelection == "Paper" && computerSelection == "Rock"
-    || playerSelection == "Scissors" && computerSelection == "Paper") {
-    playerScore += 1;
-
-    alert(`You Win! ${playerSelection} beats ${computerSelection}`);
-  } else {
-    computerScore += 1;
-
-    alert(`You Lose! ${computerSelection} beats ${playerSelection}`);
-  }
-}
-
-//function to play selected number of games
-function game () {
-  for (i = 1; i <= numberGames; i++) {
-    playRound();
-  }
-
-  return winLogic();
-}
-
-//logic function to announce winner at end of all rounds.
-function winLogic() {
-  if (playerScore > computerScore) {
-    return `After ${gamesPlayed} rounds... You have won!`;
-  } else if (playerScore < computerScore) {
-    return `After ${gamesPlayed} rounds... You have lost!`;
-  } else {
-    return `After ${gamesPlayed} rounds... You have a tie game!`;
-  }
-}
-
-//User selection function
-function playerChoice() {
-  playerSelection = prompt("Type Rock, Paper, or Scissors", "Type here...")
-
-  //sanitize playerSelection
+  // sanitize playerSelection
   let playerFirstLet = playerSelection[0].toUpperCase();
   let playerRemainLet = playerSelection.slice(1).toLowerCase();
   let cleanPlayerSelection = playerFirstLet + playerRemainLet;
   playerSelection = cleanPlayerSelection;
 
-  //confirm user seleciton is valid
-  if (playerSelection === "Rock" 
-  || playerSelection === "Paper" 
-  || playerSelection === "Scissors") {
+  //display player selection
+  player.textContent = (`Player plays ${playerSelection}`)
+  computerPlay();
 
-    return;
-  } else {
+  gamesPlayed += 1;
+  roundCounterDisplay.textContent = gamesPlayed;
 
-    alert("Invalid selection... Try Again!");
-    playerChoice();
+  roundLogic();
+
+  if (gamesPlayed >= 5) {
+    winLogic();
   }
 }
 
+//Logic for single round of game
+function roundLogic() {
+  if (playerSelection == computerSelection) {
+    // tieCounter += 1;
+    // tieCounterDisplay.textContent = tieCounter;
+    resultDisplay.textContent = ("You are tied!");
+
+  } else if (playerSelection == "Rock" && computerSelection == "Scissors"
+    || playerSelection == "Paper" && computerSelection == "Rock"
+    || playerSelection == "Scissors" && computerSelection == "Paper") {
+    playerScore += 1;
+    playerScoreDisplay.textContent = playerScore;
+    resultDisplay.textContent = (`You Win! ${playerSelection} beats ${computerSelection}`);
+  
+  } else {
+    computerScore += 1;
+    computerScoreDisplay.textContent = computerScore;
+
+    resultDisplay.textContent = (`You Lose! ${computerSelection} beats ${playerSelection}`);
+  }
+}
+
+// //function to play selected number of games
+// function game () {
+//   // for (i = 1; i <= numberGames; i++) {
+//     playRound();
+//   // }
+
+//   return winLogic();
+// }
+
+//logic function to announce winner at end of all rounds.
+function winLogic() {
+  if (playerScore > computerScore) {
+    resultDisplay.textContent = `After ${gamesPlayed} rounds... You have won!`;
+  } else if (playerScore < computerScore) {
+    resultDisplay.textContent = `After ${gamesPlayed} rounds... You have lost!`;
+  } else {
+    resultDisplay.textContent = `After ${gamesPlayed} rounds... You have a tie game!`;
+  }
+}
+
+//User selection function
+// function playerChoice(id) {
+//   playerSelection = id;
+//   console.log(`Player selects ${playerSelection}`);
+
+  // if (playerSelection === null) {
+  //   playerChoice();
+  // }
+
+  // //confirm user selection is valid
+  // if (playerSelection === "Rock" 
+  // || playerSelection === "Paper" 
+  // || playerSelection === "Scissors") {
+
+  //   return;
+  // } else {
+
+  //   alert("Invalid selection... Try Again!");
+  //   playerChoice();
+  // }
+// }
+
 //initiate game start via alert upon loading.
-alert(game());
+// alert(game());
 
+//buttons setup
+const button = document.querySelectorAll('.button');
+button.forEach(button => button.addEventListener('click', buttonClicked));
 
+function buttonClicked (e) {
+  let id = e.target.id;
+  
+  if (gamesPlayed < numberGames) {
+    playRound(id);
+  }
+}
+
+//Div Displays
+const roundCounterDisplay = document.querySelector('#round-counter');
+// const tieCounterDisplay = document.querySelector('#tie-counter');
+const playerScoreDisplay = document.querySelector('#player-score');
+const computerScoreDisplay = document.querySelector('#computer-score');
+const playerDisplay = document.querySelector('#player');
+const computerDisplay = document.querySelector('#computer');
+const resultDisplay = document.querySelector('#result');
 
